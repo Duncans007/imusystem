958f1424ed2f4652d83d053a76bbf69a012de36a
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-13 15:30:30 -0400

    Update 2LegMain.py

--- 2LegMain.py
+++ 2LegMain.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+#Importing Custom Functions
 from gaitDetectClass import * #class gaitDetect, func testVal(shank gyZ, heel gyZ)
 from packageHandlerFunc import * #package_handler_raw(tup)
 from sensorClass import * #class sensorObject, newValues(valueArray), angularAccCalc(), angleCalc(gaitDetectObject)
 from serialSend import * #ardno(msg as string)
 from slipAlgorithmFunc import * #slipAlgorithm(pelvis_forward_acc, heel_forward_acc, L_hh)
 
+#Importing python libraries
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
 import serial
@@ -13,6 +15,7 @@ import time
 from math import sin, cos, sqrt, atan2
 import numpy as np
 
+#These will all be passed, rather than global eventually.
 global timeCurrent, varType, timeStart
 global dataDict, flagDict, toggleFlagDict
 global packetReady, rPacketReady, passToAlgorithm
@@ -25,6 +28,7 @@ global hip_heel_length
 globall intelNUCserial
 
 #-----------------------------------#
+#USER INPUTS
 ip = "localhost"
 port = 6565
 
@@ -38,7 +42,7 @@ mass = 60 #kg
 height = 180 #cm
 #-----------------------------------#
 
-
+#Turns data collection for particular sensors on/off if necessary.
 toggleFlagDict = {
     "rThigh": True,
     "rShank": True,
@@ -113,6 +117,7 @@ passToAlgorithm = {
 }
 
 
+#Function to handle OSC input
 def data_handler(address, *args):
     global timeCurrent, varType, timeStart
     global dataDict, flagDict, toggleFlagDict
@@ -127,13 +132,13 @@ def data_handler(address, *args):
     
     out = []
     
+#Collects variable type and sensor address as numbers
     varType = address[10]
-    
     addr = ''
     addr += str(address[len(address) - 3])
     addr += str(address[len(address) - 1])
     
-    #Takes in individual data and assembles into packages
+    #Takes in individual data and assembles into easily indexable dictionary packages.
     if addr in addressDict:
         limb = addressDict[addr]
 
@@ -141,23 +146,24 @@ def data_handler(address, *args):
             dataDict[limb] = package_handler_raw(args)
             flagDict[limb] = True
 
-#SERIAL SEND
+#SERIAL SEND------------------------------------------------
+#Whenever new data is brought in, it cuts the magnetometer (change 0:6 to 0:9 if you want magnetometer included), inserts the 2-letter sensor code at the beginning, and sends it with function to intelNUCserial.
+#To skip, comment send_over_serial()
             serialArr = dataDict[limb]
 			serialArr = serialArr[0:6]
 			serialArr.insert(0,limb[0:2])
             send_over_serial(serialArr, intelNUCserial)
+#-----------------------------------------------------------
 
-        if flagDict == toggleFlagDict:
-            for x in range(len(flagDict) - 1):
-                out.append(dataDict[orderDict[x]])
-                    
+#Tests if all sensors have been received before assembling packet and sending to algorithm
+        if flagDict == toggleFlagDict:                    
             for x in flagDict:
                 flagDict[x] = False
             
             rPacketReady = True
     
     
-    #Assembles full packet for sending to Algorithm
+#Assembles full packet so that entire IMU state can be analyzed by algorithm at once.
     if rPacketReady:
         rPacketReady = False
         
@@ -171,17 +177,18 @@ def data_handler(address, *args):
         
         packetReady = True
         
-    #-------------------------------------------------------------#    
-    #Code is broken into reader above and algorithm below for increased customization and ease of changing algorithm.
+#----------------------------------------------------------------------------------------------------------------#    
+#Code is broken into reader above and algorithm below for increased customization and ease of changing algorithm. Everything below this line is almost entirely customizable.
+#If sensor orientations change, they can be changed in the code below.
         
-    
+#When complete system state is ready, run calculations
     if packetReady:
         packetReady = False
         timeLastRun = timeCurrent
         timeCurrent = time.time()
         timeToRun = timeCurrent - timeLastRun
         
-        #Update data
+#Update data
         rt_raw = passToAlgorithm['rt_raw']
         rs_raw = passToAlgorithm['rs_raw']
         rh_raw = passToAlgorithm['rh_raw']
@@ -190,11 +197,11 @@ def data_handler(address, *args):
         lh_raw = passToAlgorithm['lh_raw']
         b_raw  = passToAlgorithm['b_raw']
         
-        #Values are moved around to equalize the axes of the IMUs.
-        #Does not put IMUs on a global coordinate system.
-        #Only sets local axes to be the same.
+#Values are moved around to equalize the axes of the IMUs.
+#Does not put IMUs on a global coordinate system.
+#Only sets local axes to be the same for ease on analysis.
 		
-        #Right Thigh - no flipped values
+#Right Thigh - no flipped values
         objRThigh.gyX = rt_raw[0]
         objRThigh.gyY = rt_raw[1]
         objRThigh.gyZ = rt_raw[2]
@@ -207,7 +214,7 @@ def data_handler(address, *args):
         objRThigh.mgY = rt_raw[7]
         objRThigh.mgZ = rt_raw[8]
         
-        #Right Shank - no flipped values
+#Right Shank - no flipped values
         objRShank.gyX = rs_raw[0]
         objRShank.gyY = rs_raw[1]
         objRShank.gyZ = rs_raw[2]
@@ -220,7 +227,7 @@ def data_handler(address, *args):
         objRShank.mgY = rs_raw[7]
         objRShank.mgZ = rs_raw[8]
         
-        #Right Heel - X and Y axes flipped, X negated
+#Right Heel - X and Y axes flipped, X negated
         objRHeel.gyX = -rh_raw[1]
         objRHeel.gyY = rh_raw[0]
         objRHeel.gyZ = rh_raw[2]
@@ -233,7 +240,7 @@ def data_handler(address, *args):
         objRHeel.mgY = rh_raw[6]
         objRHeel.mgZ = rh_raw[8]
 		
-        #Left Thigh - X and Z values negated
+#Left Thigh - X and Z values negated
         objLThigh.gyX = -lt_raw[0]
         objLThigh.gyY =  lt_raw[1]
         objLThigh.gyZ = -lt_raw[2]
@@ -246,7 +253,7 @@ def data_handler(address, *args):
         objLThigh.mgY =  lt_raw[7]
         objLThigh.mgZ = -lt_raw[8]
         
-        #Left Shank - X and Z values negated		
+#Left Shank - X and Z values negated		
         objLShank.gyX = -ls_raw[0]
         objLShank.gyY =  ls_raw[1]
         objLShank.gyZ = -ls_raw[2]
@@ -259,7 +266,7 @@ def data_handler(address, *args):
         objLShank.mgY =  ls_raw[7]
         objLShank.mgZ = -ls_raw[8]
                   
-        #Left Heel - X and Y axes flipped, then X, Y, and Z values negated.
+#Left Heel - X and Y axes flipped, then X, Y, and Z values negated.
         objLHeel.gyX = -lh_raw[1]
         objLHeel.gyY =  lh_raw[0]
         objLHeel.gyZ = -lh_raw[2]
@@ -272,7 +279,7 @@ def data_handler(address, *args):
         objLHeel.mgY =  lh_raw[6]
         objLHeel.mgZ = -lh_raw[8]
         
-        #Lower Back - X and Z values flipped, X value negated
+#Lower Back - X and Z values flipped, X value negated
         objLowBack.gyX = -b_raw[2]
         objLowBack.gyY =  b_raw[1]
         objLowBack.gyZ =  b_raw[0]
@@ -286,22 +293,25 @@ def data_handler(address, *args):
         objLowBack.mgZ =  b_raw[6]
         
         
-        #Run actual calculations contained in the objects
-        
-        objRThigh.angleCalc(gaitDetectRight)
+#Run actual calculations contained in the objects
+#Right and Left Gait Detection
+        gaitDetectRight.testVal(objRShank.gyZ, objRHeel.gyZ)
+        gaitDetectLeft.testVal(objLShank.gyZ, objLHeel.gyZ)
+
+#Right Leg Angle Approximations
+	objRThigh.angleCalc(gaitDetectRight)
         objRShank.angleCalc(gaitDetectRight)
         objRHeel.angleCalc(gaitDetectRight)
-		
+
+#Left Leg Angle Approximations
         objLThigh.angleCalc(gaitDetectLeft)
         objLShank.angleCalc(gaitDetectLeft)
         objLHeel.angleCalc(gaitDetectLeft)
-        
-        gaitDetectRight.testVal(objRShank.gyZ, objRHeel.gyZ)
-        gaitDetectLeft.testVal(objLShank.gyZ, objLHeel.gyZ)
 		
-        
+#Append beginning of output string - time, time between measurements, right gait stage, left gait stage
         outputString = f"{time.time() - timeStart}\t{timeToRun}\t{gaitDetectRight.gaitStage}\t{gaitDetectLeft.gaitStage}\t\t"
-		
+
+#Cycle through all sensor objects to append formatted version of every sensor's raw data to output string
         for x in objects:
             outputString += f"{x.gyX}\t"
             outputString += f"{x.gyY}\t"
@@ -315,33 +325,38 @@ def data_handler(address, *args):
             outputString += f"{x.mgY}\t"
             outputString += f"{x.mgZ}\t\t\t"
 
+#Cycle through all sensor objects to append formatted version of every sensor's angle approximation to output string (note, back angle is not calculated and stays at 0)
         for x in objects:
             outputString += f"{x.zAngle}\t"
+        
+#Calculates Slip Indicator from Trkov IFAC 2017 paper
+        slipRight = gaitDetectRight.slipTrkov(objLowBack.acX, ((objRHeel.acX * np.cos(objRHeel.zAngle * .01745)) - (objRHeel.acY * np.sin(objRHeel.zAngle * .01745))), hip_heel_length)
+        slipLeft = gaitDetectLeft.slipTrkov(objLowBack.acX, ((objLHeel.acX * np.cos(objLHeel.zAngle * .01745)) - (objLHeel.acY * np.sin(objLHeel.zAngle * .01745))), hip_heel_length)
+		
+#Appends final values to output string, print, and save to file.
+        outputString += f"{slipRight}\t{slipLeft}\t"
+        outputString += f"\n"
 		
         print(outputString)
         fileDump.write(f"{outputString}")
-        
-		slipRight = gaitDetectRight.slipTrkov(objLowBack.acX, ((objRHeel.acX * np.cos(objRHeel.zAngle * .01745)) - (objRHeel.acY * np.sin(objRHeel.zAngle * .01745))), hip_heel_length)
-		slipLeft = gaitDetectLeft.slipTrkov(objLowBack.acX, ((objLHeel.acX * np.cos(objLHeel.zAngle * .01745)) - (objLHeel.acY * np.sin(objLHeel.zAngle * .01745))), hip_heel_length)
-		
-        fileDump.write(f"{slipRight}\t{slipLeft}\t")
-        fileDump.write("\n")
 		
 
-        #SERIAL SEND
+#SERIAL SEND--------------------------------------------
+#Sends all processed data over serial.
+#comment send_over_serial to skip
         serialArr = ["PR", time.time() - timeStart}, {timeToRun}, {gaitDetectRight.gaitStage}, {gaitDetectLeft.gaitStage}, slipRight / (10**26), slipLeft / (10**26)]
         send_over_serial(serialArr, intelNUCserial)
+#---------------------------------------------------
 
 
-
+#Handles any OSC messages that aren't picked up by dataHandler
 def default_handler(address, *args):
     out = 'DEFAULT '
     out += str(address)
     out += str(args)
-    #print(out)
 
 
-
+#Sets up OSC server
 def main_func(ip, port):   
     dispatcher = Dispatcher()
     dispatcher.map("/Chordata/r*", data_handler)
@@ -353,8 +368,12 @@ def main_func(ip, port):
 
 
 if __name__ == "__main__":    
-    intelNUCserial = serial.Serial(intelNUCport, intelNUCbaud)
+    
+	#Variable initializations
+	#serial object for NUC. Comment out if not used.
+	intelNUCserial = serial.Serial(intelNUCport, intelNUCbaud)
 	
+	#create objects for sensor operations and value storage.
 	objRThigh = sensorObject()
     objRShank = sensorObject()
     objRHeel = sensorObject()
@@ -365,15 +384,17 @@ if __name__ == "__main__":
 	
     objLowBack = sensorObject()
     
+	#create gait detect objects for each leg
     gaitDetectRight = gaitDetect()
     gaitDetectLeft = gaitDetect()
 	
+	#create lists that can be cycles through to iterate over every object, as well as create the file data header.
     objects = [objRThigh, objRShank, objRHeel, objLThigh, objLShank, objLHeel, objLowBack]
     stringObjects = ["RThigh", "RShank", "RHeel", "LThigh", "LShank", "LHeel", "LowBack"]
     stringAxes = ["x","y","z"]
     stringSensors = ["gy","ac","mg"]
 	
-	
+	#Create formatted file header
     fileDump = open("algDump.txt", "w+")
     header = "time\ttimeToRun\tgaitStageR\tgaitStageL\t\t"
     for x in stringObjects:
