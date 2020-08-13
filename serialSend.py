e75ca4ff212954cac2d9d07ae0f009efc7815f10
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-13 17:12:18 -0400

    Update serialSend.py

--- serialSend.py
+++ serialSend.py
@@ -6,17 +6,21 @@ def ardno(msg):
     
 def send_over_serial(msgArray, serialSend):
     sendStr = ""
+    
+    #Pools sensor values into string for conversion to bytes
     for n in msgArray:
         sendStr += f"{truncate(n,2)},"
     
+    #Cut last comma, add terminating character instead
     sendStr = sendStr[:-1]
     sendStr += f"\n"
     
+    #Encode with UTF-8 and send over serial.
     serialSend.flush()
     serialSend.write(sendStr.encode('utf-8'))
     
     
-
+#Cuts number to "digits" number of decimal points.
 def truncate(number, digits) -> float:
     import math
     stepper = 10.0 ** digits
