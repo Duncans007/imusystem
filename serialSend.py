0b7851a696a74c7410f3fddd8d3d9309188c23c9
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-13 01:35:39 -0400

    Update serialSend.py

--- serialSend.py
+++ serialSend.py
@@ -1,4 +1,22 @@
-def send_over_serial(msg):
+def ardno(msg):
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     ser.write(b"{}".format(msg))
+
+    
+def send_over_serial(msgArray, serialSend):
+    sendStr = ""
+    for n in msgArray:
+        sendStr += f"{n},"
+    
+    sendStr = sendStr[:-1]
+    sendStr += f"\n"
+    
+    serialSend.flush()
+    serialSend.write(sendStr.encode('utf-8'))
+    
+    
+import math
+def truncate(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
