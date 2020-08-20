7680d7200448b728df728cc4faf05cfa3e0502f8
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-20 12:06:43 -0400

    Update serialSend.py

--- serialSend.py
+++ serialSend.py
@@ -1,6 +1,5 @@
 def ardno(msg):
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
     ser.write(b"{}".format(msg))
 
     
@@ -21,7 +20,6 @@ def send_over_serial(msgArray, serialSend):
     sendStr += f"\n"
     
     #Encode with UTF-8 and send over serial.
-    serialSend.flush()
     serialSend.write(sendStr.encode('utf-8'))
     
     
