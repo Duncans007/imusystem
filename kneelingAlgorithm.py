ae281ab4344956f90b226979c8bd21867315b074
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-08-29 22:43:15 -0400

    added torque window detection

--- kneelingAlgorithm.py
+++ kneelingAlgorithm.py
@@ -8,11 +8,16 @@ class kneelingDetection:
         self.movingAvgGyThighL = []
         self.Rcounter = 0
         self.Lcounter = 0
+        self.isKneeling = False
         
         #Perpetual values for torqueEstimation()
         self.A = 0.012
         self.B = -0.002
         self.C = -0.075
+        
+        #torqueWindow()
+        self.deliverTorque = False
+        self.timeLastKneeling = 0
 
     def torqueEstimation(self, NMKG, mass, kneeAngle, angVel):
         #NMKG - Newton-meters per kilogram (for initial tests, 0.15, 0.30, 0.45)
@@ -23,7 +28,19 @@ class kneelingDetection:
         torqueOutput = (self.A * kneeAngleCorrected) + (self.B * angVel) + self.C
         torqueOutput = torqueOutput * NMKG * mass * (12/15)
         
-        return torqueOutput
+        if self.torqueWindow(kneeAngle):
+            return torqueOutput
+        else:
+            return 0
+    
+    def torqueWindow(self, kneeAngle):
+        import time
+        if self.isKneeling == True or (time.time() - self.timeLastKneeling < .5 and kneeAngle < 170):
+            self.deliverTorque = True
+            if self.isKneeling == True:
+                self.timeLastKneeling = time.time()
+        else:
+            self.deliverTorque = False
         
     def kneelingDetection(self, thighObjR, shankObjR, heelObjR, thighObjL, shankObjL, heelObjL):
         import numpy as np
@@ -66,9 +83,9 @@ class kneelingDetection:
         
     #Test if angle is past a rather large and easy to determine threshold (60 degrees from straight)
         if (leftKneeAngle < 120) and (rightKneeAngle < 120):
-            isKneeling = True
+            self.isKneeling = True
         else:
-            isKneeling = False
+            self.isKneeling = False
             legForward = "X"
             
             
@@ -76,7 +93,7 @@ class kneelingDetection:
     #Leg with horizontal shin is backwards, if both shins horizontal then both legs down.
     #To expand for kneeling on an angle, use the difference between the shin angles with a window for how close they can be, and the lesser/greater one is forward once it passes the threshold
         
-        if isKneeling == True:
+        if self.isKneeling == True:
             legForwardThreshold = 30
             if abs(shankAngleR - shankAngleL) < legForwardThreshold:
                 legForward = "2"
