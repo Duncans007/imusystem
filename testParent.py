7835c6b78c633003a0700ed9cfea68f9101dd485
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 19:12:49 -0400

    Update testParent.py

--- testParent.py
+++ testParent.py
@@ -2,11 +2,11 @@
 
 from multiprocessing import Process,Queue,Pipe
 import time
-from testChild import f
+from CUNYreceiver import async_teensy
 
 if __name__ == '__main__':
     parent_conn,child_conn = Pipe()
-    p = Process(target=f, args=(child_conn,))
+    p = Process(target=async_teensy, args=(child_conn,))
     p.start()
     while True:
         print(parent_conn.recv())   # prints "Hello"
