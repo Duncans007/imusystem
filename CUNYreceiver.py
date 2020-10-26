4c9864029a8675105206c6300c0f2b694d07f064
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 19:11:22 -0400

    Update CUNYreceiver.py

--- CUNYreceiver.py
+++ CUNYreceiver.py
@@ -3,12 +3,21 @@
 from multiprocessing import Process,Pipe
 
 def async_teensy(child_conn, teensyPort):
-    msg = 1
+    msg = {
+        "yes" : 1,
+        "no" : 2,
+        "maybe" : 3
+    }
+    
     while True:
-        receivedData, outputArray = receive_from_teensy(teensyPort)
+        #receivedData, outputArray = receive_from_teensy(teensyPort)
         if receivedData:
-            send_to_teensy(1, 1, teensyPort)
-            child_conn.send(outputArray)
+            #send_to_teensy(1, 1, teensyPort)
+            #child_conn.send(outputArray)
+            child_conn.send(msg)
+            msg["yes"] += 1
+            msg["no"] += 2
+            msg["maybe"] += 3
                 
 
     child_conn.close()
