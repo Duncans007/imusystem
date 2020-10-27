0ba693f02fd2b02f616f69a9d6fda411af90718a
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 20:54:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Both arguments are True/False, capital first letter. First is NUC. Second 
 
 sleep 1
 
-./2LegMain.py -$1 -$2 && (cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)
+./2LegMain.py $1 $2 && (cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)
