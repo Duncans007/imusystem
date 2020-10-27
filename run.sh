5a468a5bad0cb02d368d9b13ed3f631b91c623af
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 21:30:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ echo "Arg1: NUC, Arg2: Teensy. Both arguments are true/false all lowercase."
 
 sleep 1
 
-(cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost) && ./2LegMain.py $1 $2
+(cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)&
+./2LegMain.py $1 $2
