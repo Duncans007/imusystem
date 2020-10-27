7f69358f195ab2d3a4a25033aebd8dbffd580624
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 20:53:49 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 echo "Both arguments are True/False, capital first letter. First is NUC. Second is Teensy."
 
-sleep(1)
+sleep 1
 
 ./2LegMain.py -$1 -$2 && (cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)
