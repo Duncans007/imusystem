b2a820574d0ac5b8b60ce2e9969d7ef5befdf4f5
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-26 20:17:36 -0400

    Update 2LegMain.py

--- 2LegMain.py
+++ 2LegMain.py
@@ -390,7 +390,7 @@ def data_handler(address, *args):
             
             if teensySend:
                 for i in cuny_data.items():
-                    serialArr += i[1]
+                    serialArr.append(i[1])
                     
                     
             print(serialArr)
