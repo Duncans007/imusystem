8cae292d1f36295191bd98cb954a0b975496f88b
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-30 13:21:47 -0400

    Create NUCreceiver.py

---
+++ NUCreceiver.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process,Pipe
+from serialSend import *
+
+def async_nuc(child_conn, nucPort):
+    nuc_data = {
+        "KneeL"  : 0,
+        "KneeR"  : 0,
+
+    }
+    
+    while True:
+        receivedData, outputArray = receive_from_nuc(nucPort)
+        if receivedData:
+            
+            try:
+                nuc_data["KneeL"]  = outputArray[0]
+                nuc_data["KneeR"]  = outputArray[1]
+            except IndexError:
+                pass
+            
+            
+
+            child_conn.send(nuc_data)
+                
+
+    child_conn.close()
