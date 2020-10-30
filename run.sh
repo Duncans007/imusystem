c7f36bfca3b4af559d194bfe7334761bba83ccc3
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-30 15:40:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ trap "kill 0" EXIT
 
 echo "Arg1: NUC, Arg2: Teensy, Arg3: VICON. Both arguments are true/false all lowercase."
 
-sleep 1
+sleep 5
 
 (cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)&
 ./2LegMain.py $1 $2 $3 >/dev/null
