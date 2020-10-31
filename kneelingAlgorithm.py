12c500cf039ea4ad4d275205ed74ff0cba4c5b31
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-10-31 18:22:19 -0400

    Update kneelingAlgorithm.py

--- kneelingAlgorithm.py
+++ kneelingAlgorithm.py
@@ -7,7 +7,7 @@ class kneelingDetection:
         self.NMKG = NMKG
         self.mass = mass
         self.height = height
-        self.alpha = .0005
+        self.alpha = .0007
         
         #Inputs updated on the first loop
         self.thighAngleR = 0
