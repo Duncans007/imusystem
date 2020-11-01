e07ca763b8eadd2bb5faf76149f3076600e5f720
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-01 16:40:37 -0500

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
+#Torque Controller
 mass = 80 #kg
 height = 1.80 #m
 torqueCutoff = 18 #Nm
 alpha = 1 #proportionality
+
+
+#Serial Send/Receive
+#intelNUCport = "/dev/ttyUSB0"
+intelNUCport = "/dev/ttyS0"
+intelNUCbaud = 256000
+
+
+teensyPort = "/dev/ttyS0"
+teensyBaud = 256000
+
+hip_heel_length = 1 #meters
+NMKG = 0.15
