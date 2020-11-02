6c5662d865d6d43dafa3e2eb1e2fbb993bcc8ca1
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-02 18:06:45 -0500

    Update sensorClass.py

--- sensorClass.py
+++ sensorClass.py
@@ -225,7 +225,7 @@ class sensorObject:
         #self.angleFromGravity = math.degrees(math.acos(dotProd))
         
         self.angleFromGravity = -math.degrees(math.atan2(self.acX, -self.acY))
-        self.angleFromGravity = self.angleFromGravity * ((abs(self.zAngle) + 0.00001) / (self.zAngle + 0.00001))
+        self.angleFromGravity = self.angleFromGravity * ((abs(self.acX) + 0.00001) / (self.acX + 0.00001))
         
     #Populates and minimizes array of values to smooth curve. Usually set around 2-3 frames to minimize time delay while still smoothing peaks as much as possible.
     #2-3 frames adds up to .04-.06 seconds delay total, halved and averaged comes out to a .025 second (or approximately 1 frame) delay due to this method of smoothing.
