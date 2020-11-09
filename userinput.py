aa74e22b6508fc73ace8198d8a7b4c695e9d3c35
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-09 07:25:56 -0500

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -10,13 +10,13 @@ alpha = .1 #proportionality
 #Serial Send/Receive to NUC and Simulink
 #intelNUCport = "/dev/ttyUSB0"
 intelNUCport = "/dev/ttyS0"
-intelNUCbaud = 256000
+intelNUCbaud = 115200
 
 
 #Serial Send/Receive to CUNY Teensy
 #teensyPort = "/dev/ttyACM0"
 teensyPort = "/dev/ttyS0"
-teensyBaud = 256000
+teensyBaud = 115200
 
 
 hip_heel_length = 1 #meters
