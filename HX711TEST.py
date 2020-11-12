02dad1b120c9f35d359ca7ab6a3288a38c1da0ac
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-12 16:43:59 -0500

    Create HX711TEST.py

---
+++ HX711TEST.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from hx711 import HX711
+from time import sleep
+
+while True:
+    try:
+        hx711 = HX711(
+            dout_pin=5,
+            pd_sck_pin=6,
+            channel='A',
+            gain=64
+        )
+
+        hx711.reset()   # Before we start, reset the HX711 (not obligate)
+        measures = hx711.get_raw_data(num_measures=2)
+    finally:
+        GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+
+    print("\n".join(measures))
+    sleep(.001)
