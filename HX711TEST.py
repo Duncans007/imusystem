94dc81697d6d4bb743fa4213f18ffaedd670aa4a
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-12 17:36:03 -0500

    Update HX711TEST.py

--- HX711TEST.py
+++ HX711TEST.py
@@ -6,20 +6,20 @@ import RPi.GPIO as GPIO
 
 
 
-    try:
-        hx711 = HX711(
-            dout_pin=5,
-            pd_sck_pin=6,
-            channel='A',
-            gain=64
-        )
+try:
+    hx711 = HX711(
+        dout_pin=5,
+        pd_sck_pin=6,
+        channel='A',
+        gain=64
+    )
 
-        hx711.reset()   # Before we start, reset the HX711 (not obligate)
-        while True:
-            measures = hx711.get_raw_data(num_measures=2)
-            print(measures)
-            
-    finally:
-        GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    hx711.reset()   # Before we start, reset the HX711 (not obligate)
+    while True:
+        measures = hx711.get_raw_data(num_measures=2)
+        print(measures)
 
-    print("\n".join(measures))
+finally:
+    GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+
+print("\n".join(measures))
