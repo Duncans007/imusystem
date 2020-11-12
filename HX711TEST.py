e87243ec5cff9a1f03633067c2891c0d2ac84479
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-12 17:35:27 -0500

    Update HX711TEST.py

--- HX711TEST.py
+++ HX711TEST.py
@@ -3,10 +3,9 @@ from hx711 import HX711
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setup(5, GPIO.IN)
-GPIO.setup(6, GPIO.IN)
 
-while True:
+
+
     try:
         hx711 = HX711(
             dout_pin=5,
@@ -16,9 +15,11 @@ while True:
         )
 
         hx711.reset()   # Before we start, reset the HX711 (not obligate)
-        measures = hx711.get_raw_data(num_measures=2)
+        while True:
+            measures = hx711.get_raw_data(num_measures=2)
+            print(measures)
+            
     finally:
         GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
 
     print("\n".join(measures))
-    sleep(.001)
