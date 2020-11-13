78d95ee2ba090c59d66e000d36823d21ab6640ef
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-11-12 23:05:55 -0500

    Update 2LegMain.py

--- 2LegMain.py
+++ 2LegMain.py
@@ -289,13 +289,12 @@ def data_handler(address, *args):
         
         
         if teensySend:
-            if parent_conn_teensy.poll(0):
-                cuny_data = parent_conn_teensy.recv()
+            #if parent_conn_teensy.poll(0):
+            cuny_data = parent_conn_teensy.recv()
 
         if viconData:
-            if parent_conn_nuc.poll(0):
-                nuc_data = parent_conn_nuc.recv()
-                print(nuc_data)
+            #if parent_conn_nuc.poll(0):
+            nuc_data = parent_conn_nuc.recv()
         
         
         
