93360db528eee45e1bb70e0c49b0fee97979f092
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-12-01 14:28:23 -0500

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,38 +1,49 @@
-#!/usr/bin/env python3
-
-#Options: "pid","yusu","ramp"
-controller_type = "yusu"
-
-#General Torque Controller Parameters
-mass = 80 #kg
-height = 1.80 #m
-torqueCutoff = 18 #Nm
-NMKG = 0.15 #Nm/kg
-
-#Yu&Su Specific Proportionality Constant
-alpha = .1
-
-#Ramping Specific Constants
-ramping_delay_time = 5 #seconds
-ramping_hold_time = 5 #seconds
-ramping_slope = 20 #Nm/s
+#!/usr/bin/env python3	
 
 
-#Serial Send/Receive to NUC and Simulink
-#intelNUCport = "/dev/ttyUSB0"
-intelNUCport = "/dev/ttyS0"
-intelNUCbaud = 256000
+#Options: "pid","yusu","ramp"	
+controller_type = "yusu"	
 
 
-#Serial Send/Receive to CUNY Teensy
-teensyPort = "/dev/ttyACM0"
+#General Torque Controller Parameters	
+mass = 80 #kg	
+height = 1.80 #m	
+torqueCutoff = 30 #Nm	
+NMKG = 0.3 #Nm/kg	
+lowBackAngleShift = 18	
+
+
+#Yu&Su Specific Proportionality Constant	
+alpha = .1	
+alpha2 = 0.15	
+SecondsToChange = 1	
+
+
+#Ramping Specific Constants	
+ramping_delay_time = 5 #seconds	
+ramping_hold_time = 5 #seconds	
+ramping_slope = 20 #Nm/s	
+
+
+#Serial Send/Receive to NUC and Simulink	
+intelNUCport = "/dev/ttyUSB0"	
+#intelNUCport = "/dev/ttyS0"	
+intelNUCbaud = 115200	
+
+
+#Serial Send/Receive to CUNY Teensy	
+teensyPort = "/dev/ttyACM0"	
 #teensyPort = "/dev/ttyS0"
-teensyBaud = 256000
-addGravityToTorque = False
+teensyBaud = 115200	
 
-sensorCalibTime = 1
-angleCalibTime = 2
 
-hip_heel_length = 1 #meters
+#Configure calibration times
+sensorCalibTime = 2
+angleCalibTime = 2	
 
+
+#Turn upper back sensor on or off
 sensor8 = False
+
+
+hip_heel_length = 1 #meters	
