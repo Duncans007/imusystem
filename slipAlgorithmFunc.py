6ccb42958b2602c5d2960fb9e1dc54f735b6693e
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-01-18 14:29:41 -0500

    Update slipAlgorithmFunc.py

--- slipAlgorithmFunc.py
+++ slipAlgorithmFunc.py
@@ -1,6 +1,9 @@
 def slipAlgorithm(pelvisAcc, forwardFootAcc, L_hh):
+    #Update notes: double check that L_hh pulls from userinput.py
+
     global fileDump
 
+    #Constants from paper Trkov "sensor based slip detection..." IEEE '19
     slip_constant = 2.83 #or 1.87
     beta = 2.718
     gamma = 40 #-562 or -377 #deg/s	
