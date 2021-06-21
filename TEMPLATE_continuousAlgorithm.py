51d899741741bb551ff6b9161fa56b918147b458
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-06-21 14:57:54 -0400

    Create TEMPLATE_continuousAlgorithm.py

---
+++ TEMPLATE_continuousAlgorithm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#Create object (must be run *once* before running function to create object)
+class algorithmName:
+    
+    #Runs once on object creation
+    def __init__(self, init_variable_1):
+        
+        #Define constants at object creation
+        self.gravity = 9.81
+        
+        #Define input constants at object creation
+        self.init_var = init_variable_1
+        
+        #Define any perpetual variables
+        self.movingAvgArr = [0];
+        
+                
+    def input_new_values_and_run_calculation(self, variable_input_1):
+        #Example: Moving Average calculation
+        
+        #Add function input to array
+        self.movingAvgArr.append(variable_input_1)
+        
+        #Keep array size limited
+        if len(self.movingAvgArr) > 10:
+            self.movingAvgArr.pop(0)
+        
+        #Return moving average
+        return mean(self.movingAvgArr)
+    
+    
+#To insert into main code:
+# under "if __name__ == "__main__": Variable initializations", insert object creation --> [ objectName = algorithmName(init_variables) ]
+# under "algorithms and secondary angle calculations", insert function call --> [ output = objectName.input_new_values_and_run_calculation(input) ]
+#Add output to file
+# immediately before "header += f"\n"  fileDump.write(header)", add column title --> [ header += f"\tTITLE" ]
+# under "DATA OUTPUT ----------", before "fileDump.write(f"{outputString}")", add output --> [ outputString += f"\t{output} ]
