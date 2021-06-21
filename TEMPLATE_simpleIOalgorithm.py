bf56ed69916402c961d244454d334b8d74b50b89
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-06-21 14:23:38 -0400

    Create TEMPLATE_simpleIOalgorithm.py

---
+++ TEMPLATE_simpleIOalgorithm.py
@@ -0,0 +1,10 @@
+#Name algorithm and define inputs
+def algorithmName(alg_input_1, alg_input_2):
+    
+    #Define Constants
+    constant_gravity = 9.81
+    
+    #Write calculations
+    alg_output_1 = alg_input_1 + alg_input_2
+    
+    return alg_output_1
