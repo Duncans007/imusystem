1d753125ccc7668126e66351c905b86a53adf52c
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-07-18 22:01:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-echo "Arg1: NUC, Arg2: Teensy, Arg3: VICON. Both arguments are true/false all lowercase."
+#echo "Arg1: NUC, Arg2: Teensy, Arg3: VICON. Both arguments are true/false all lowercase."
 
-sleep 1
+#sleep 1
 
 (cd ../notochord/bin ; ./notochord --scan --raw --no_bundles --odr=97 -y localhost)&
 
 sleep 6
 
-./variableSensorMain.py $1 $2 $3 #>/dev/null
+./variableSensorMain.py #$1 $2 $3 #>/dev/null
