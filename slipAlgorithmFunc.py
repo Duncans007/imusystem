cd6391380923e55ab41b0ad8cc8a23a39a692569
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-08-26 15:57:32 -0400

    removed unused variables

--- slipAlgorithmFunc.py
+++ slipAlgorithmFunc.py
@@ -2,8 +2,6 @@
 def slipAlgorithm(pelvisAcc, forwardFootAcc, L_hh):
     #Update notes: double check that L_hh pulls from userinput.py
 
-    global fileDump
-
     #Constants from paper Trkov "sensor based slip detection..." IEEE '19
     slip_constant = 2.83 #or 1.87
     beta = 2.718
