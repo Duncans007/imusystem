88ed4dbcb745a357d0c83090fe2f64120ad8db7f
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-08-26 16:18:58 -0400

    added comments, moved hip_heel_length to trkov slip detection section

--- userinput.py
+++ userinput.py
@@ -4,7 +4,7 @@
 processing_frequency = 50
 
 #Activate/deactivate individual sensor recording
-#Note that disabling some (e.g. shank) will cause some detectors to not work
+#Note that disabling some (e.g. shank) will cause some detectors to throw errors
 toggle_rThigh  = True
 toggle_rShank  = True
 toggle_rHeel   = True
@@ -19,6 +19,9 @@ toggle_topBack = True
 sensorCalibTime = 2 #Calib time to eliminate gyro noise.
 angleCalibTime = 2 #Calib time (after finishing sensorCalibTime) to zero angle measurements.
 
+#Hip to heel length for Trkov slip detection (m)
+hip_heel_length = 1
+
 
 #Torque Controller Options: 
 # "pid" - DS developed, uses knee angle and thigh ang. vel.
@@ -27,17 +30,14 @@ angleCalibTime = 2 #Calib time (after finishing sensorCalibTime) to zero angle m
 # "trkov" - Developed by MT, adapted by DS, uses full lower body kinematics plus lower back.
 controller_type = "trkov"	
 
-
 #General Torque Controller Parameters	
 mass = 68 #Subject mass (kg)
 height = 1.80 #Subject height (m)
 torqueCutoff = 30 #Maximum allowable torque (Nm	)
 NMKG = 0.25 #Approximate torque per subject unit weight (Nm/kg)
 
-
 #YU&SU Controller Proportionality Constants	
 alpha = .1	
-
 
 #Ramping Specific Constants	
 ramping_delay_time = 5 #seconds	
@@ -45,15 +45,12 @@ ramping_hold_time = 5 #seconds
 ramping_slope = 20 #Nm/s	
 
 
-
 #ADDITIONAL DEVICES
-
 #Serial Send/Receive for NUC and Simulink	
 nucSend = False
 intelNUCport = "/dev/ttyUSB0"	
 #intelNUCport = "/dev/ttyS0"	
 intelNUCbaud = 115200	
-
 
 #Serial Send/Receive for CUNY Teensy	
 teensySend = False
@@ -61,30 +58,18 @@ teensyPort = "/dev/ttyACM0"
 #teensyPort = "/dev/ttyS0"
 teensyBaud = 115200	
 
-
 #Load cell on foot
-#Load cell plugs into arduino, which plugs into pi via USB port
+#Connects to arduino, which plugs into pi via USB port
 #same values used for streaming gait variables to arduino
 loadCell = False #stream loadcell variables from arduino
+
+#Arduino Gait Stream
+#Arduino can relay information directly to the computer
+#This is beneficial because it bypasses the need for a serial interpreter or wifi
+#Data can easily be captured using putty or other simple software
 streamGait = False #stream gait variables to arduino
+
+#loadCell and streamGait will both be on the same port if only one is plugged in
+#if both are plugged in, further configuration is required
 arduinoPort = "/dev/ttyACM0"
 arduinoBaud = 256000
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-hip_heel_length = 1 #meters	
