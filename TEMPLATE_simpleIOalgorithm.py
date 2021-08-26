5759f72a3313732076546780e87ae7d0de6f4f19
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-08-26 16:53:17 -0400

    clarifying instructions

--- TEMPLATE_simpleIOalgorithm.py
+++ TEMPLATE_simpleIOalgorithm.py
@@ -10,8 +10,8 @@ def algorithmName(alg_input_1, alg_input_2):
     
     return alg_output_1
 
-#Add algorithm to program:
+#To insert into main code (file variableSensorMain.py):
+#(3) line insertions in total:
 # under "algorithms and secondary angle calculations", insert function call --> [ output = algorithmName(input) ]
-#Add output to file:
 # under setup>create formatted file header, immediately before "header += f"\n"  fileDump.write(header)", add column title --> [ header += f"\tTITLE" ]
 # under "DATA OUTPUT ----------", before "fileDump.write(f"{outputString}")", add output --> [ outputString += f"\t{output} ]
