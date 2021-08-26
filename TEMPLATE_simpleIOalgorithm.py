610a4542965e3fca09d076cb3ccee40f28b52ff9
duncan006 <67116328+duncan006@users.noreply.github.com>
2021-08-26 16:49:49 -0400

    clarified instructions

--- TEMPLATE_simpleIOalgorithm.py
+++ TEMPLATE_simpleIOalgorithm.py
@@ -13,5 +13,5 @@ def algorithmName(alg_input_1, alg_input_2):
 #Add algorithm to program:
 # under "algorithms and secondary angle calculations", insert function call --> [ output = algorithmName(input) ]
 #Add output to file:
-# immediately before "header += f"\n"  fileDump.write(header)", add column title --> [ header += f"\tTITLE" ]
+# under setup>create formatted file header, immediately before "header += f"\n"  fileDump.write(header)", add column title --> [ header += f"\tTITLE" ]
 # under "DATA OUTPUT ----------", before "fileDump.write(f"{outputString}")", add output --> [ outputString += f"\t{output} ]
