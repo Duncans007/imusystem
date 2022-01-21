3628cddeb789c9dae3e701e6b5045e7022923202
Duncan <67116328+Duncans007@users.noreply.github.com>
2022-01-21 13:55:49 -0500

    added comments

--- ARDUINOreceiver.py
+++ ARDUINOreceiver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# This function is used to create an asynchronous listener that can be called by the main program. It constantly listens to data input from the Serial input configured for arduino. Typically used for Load Cell data.
+
 from multiprocessing import Process,Pipe
 from serialSend import *
 
